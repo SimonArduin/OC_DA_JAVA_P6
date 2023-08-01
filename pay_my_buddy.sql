152b126ee4277f30c2579af4cbf43c7ba01e9f61
SimonArduin <125870016+SimonArduin@users.noreply.github.com>
2023-08-01 16:17:06 +0200

    removed constraints and triggers from database

--- pay_my_buddy.sql
+++ pay_my_buddy.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `pay_my_buddy` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `pay_my_buddy`;
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: pay_my_buddy
@@ -7,13 +9,40 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utilisateur` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `mot_de_passe` varchar(100) NOT NULL,
+  `solde` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `compte_bancaire` varchar(34) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+LOCK TABLES `utilisateur` WRITE;
+/*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
+/*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `taxe`
@@ -40,33 +69,6 @@ INSERT INTO `taxe` VALUES (1,0.05);
 UNLOCK TABLES;
 
 --
--- Table structure for table `utilisateur`
---
-
-DROP TABLE IF EXISTS `utilisateur`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `utilisateur` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `mot_de_passe` varchar(100) NOT NULL,
-  `solde` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `compte_bancaire` varchar(34) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `utilisateur`
---
-
-LOCK TABLES `utilisateur` WRITE;
-/*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
-/*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `transaction`
 --
 
@@ -79,7 +81,7 @@ CREATE TABLE `transaction` (
   `description` varchar(255) DEFAULT NULL,
   `montant` decimal(10,2) NOT NULL,
   `compte_bancaire` varchar(34) DEFAULT NULL,
-  `vers_compte` tinyint(1) DEFAULT NULL,
+  `vers_compte` bit(1) DEFAULT NULL,
   `emetteur_id` int NOT NULL,
   `destinataire_id` int DEFAULT NULL,
   `taxe_id` int NOT NULL,
@@ -89,10 +91,8 @@ CREATE TABLE `transaction` (
   KEY `FK_transaction_destinataire_id` (`destinataire_id`),
   CONSTRAINT `FK_transaction_destinataire_id` FOREIGN KEY (`destinataire_id`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `FK_transaction_emetteur_id` FOREIGN KEY (`emetteur_id`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT `FK_transaction_taxe_id` FOREIGN KEY (`taxe_id`) REFERENCES `taxe` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT `CHK_transaction_montant_is_positive` CHECK ((`montant` >= 0)),
-  CONSTRAINT `CHK_transaction_vers_compte` CHECK (((`compte_bancaire` is null) xor (`vers_compte` is not null)))
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `FK_transaction_taxe_id` FOREIGN KEY (`taxe_id`) REFERENCES `taxe` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -139,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-13 14:34:30
+-- Dump completed on 2023-08-01 16:06:13
