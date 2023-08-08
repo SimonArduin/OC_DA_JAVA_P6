d7eca5d4abbffe61df54a9f912d6db8bb4351743
SimonArduin <125870016+SimonArduin@users.noreply.github.com>
2023-08-08 16:03:28 +0200

    added class diagram, added currency table

--- pay_my_buddy.sql
+++ pay_my_buddy.sql
@@ -18,6 +18,30 @@ USE `pay_my_buddy`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `monnaie`
+--
+
+DROP TABLE IF EXISTS `monnaie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `monnaie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `monnaie`
+--
+
+LOCK TABLES `monnaie` WRITE;
+/*!40000 ALTER TABLE `monnaie` DISABLE KEYS */;
+INSERT INTO `monnaie` VALUES (1,'euro');
+/*!40000 ALTER TABLE `monnaie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `utilisateur`
 --
 
@@ -29,10 +53,13 @@ CREATE TABLE `utilisateur` (
   `email` varchar(100) NOT NULL,
   `mot_de_passe` varchar(100) NOT NULL,
   `solde` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `compte_bancaire` varchar(34) NOT NULL,
+  `iban` varchar(34) NOT NULL,
+  `monnaie_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `email` (`email`),
+  KEY `monnaie_id` (`monnaie_id`),
+  CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`monnaie_id`) REFERENCES `monnaie` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +68,7 @@ CREATE TABLE `utilisateur` (
 
 LOCK TABLES `utilisateur` WRITE;
 /*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
+INSERT INTO `utilisateur` VALUES (7,'email01','mdp01',0.00,'rib01',0),(8,'email02','mdp02',0.00,'rib02',0);
 /*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,17 +108,20 @@ CREATE TABLE `transaction` (
   `timestamp` timestamp NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `montant` decimal(10,2) NOT NULL,
-  `compte_bancaire` varchar(34) DEFAULT NULL,
+  `iban` varchar(34) DEFAULT NULL,
   `vers_compte` bit(1) DEFAULT NULL,
   `emetteur_id` int NOT NULL,
   `destinataire_id` int DEFAULT NULL,
   `taxe_id` int NOT NULL,
+  `monnaie_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_transaction_emetteur_id` (`emetteur_id`),
   KEY `FK_transaction_taxe_id` (`taxe_id`),
   KEY `FK_transaction_destinataire_id` (`destinataire_id`),
+  KEY `FK_transaction_monnaie_id` (`monnaie_id`),
   CONSTRAINT `FK_transaction_destinataire_id` FOREIGN KEY (`destinataire_id`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `FK_transaction_emetteur_id` FOREIGN KEY (`emetteur_id`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `FK_transaction_monnaie_id` FOREIGN KEY (`monnaie_id`) REFERENCES `monnaie` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `FK_transaction_taxe_id` FOREIGN KEY (`taxe_id`) REFERENCES `taxe` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -139,4 +170,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-01 16:06:13
+-- Dump completed on 2023-08-08 16:00:33
